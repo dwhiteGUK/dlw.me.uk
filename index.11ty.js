8c7e1524d3218329a4ec465a404884aafa62ec5f
Darren White <dwhite@gentingcasinos.co.uk>
2020-08-08 15:23:49 +0100

    Start of home template

---
+++ index.11ty.js
@@ -0,0 +1,15 @@
+exports.data = {
+  layout: "base-layout.11ty.js",
+  title: 'Home of dlw running'
+};
+
+exports.render = function () {
+  return `
+      <div class="w-full md:w-1/2 order-1 md:order-2">
+          <div class="max-w-md leading-loose tracking-tight">
+              <h1 class="font-bold my-12">Home of dlw running</h1>
+
+              <se
+          </div>
+      </div>`;
+};
