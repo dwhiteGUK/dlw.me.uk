b90983f58fdeb974ef62f0b3bdff88b06437044d
Darren White <dwhite@gentingcasinos.co.uk>
2020-08-08 15:37:26 +0100

    [ADD] blog list to homepage

--- index.11ty.js
+++ index.11ty.js
@@ -3,13 +3,14 @@ exports.data = {
   title: 'Home of dlw running'
 };
 
-exports.render = function () {
+exports.render = function ({ collections }) {
   return `
       <div class="w-full md:w-1/2 order-1 md:order-2">
           <div class="max-w-md leading-loose tracking-tight">
               <h1 class="font-bold my-12">Home of dlw running</h1>
-
-              <se
+              <ul>
+              ${collections.blog.map((blog) => `<li><a href="${blog.url}">${blog.data.title}</a></li>`).join("\n")}
+              </ul>
           </div>
       </div>`;
 };
