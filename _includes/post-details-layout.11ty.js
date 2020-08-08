d8e0b5487b96968f9b2df814745ba5e7dbc57d36
Darren White <dwhite@gentingcasinos.co.uk>
2020-08-08 15:13:19 +0100

    ADD base template for chaining other layouts off

--- _includes/post-details-layout.11ty.js
+++ _includes/post-details-layout.11ty.js
@@ -1,65 +1,13 @@
-const head = require("./global/head.11ty.js");
-const contact = require("./global/contact.11ty.js");
-const menu = require("./global/menu.11ty.js");
-
 exports.data = {
-    blog_title: "dlw"
+    layout: "base-layout.11ty.js",
 };
 
-exports.render = function ({ blog_title, title, content }) {
-    return `<!DOCTYPE html>
-  <html lang="en">  
-  ${head(blog_title, title)}
-  <body class="bg-gray-100 text-gray-700 font-sans quicksand">
-  
-      <div class="p-6 sm:p-10 md:p-16 flex flex-wrap">
-            ${menu()}
-          <div class="w-full md:w-1/2 order-1 md:order-2">
-              <div class="max-w-md leading-loose tracking-tight">
-                  <h1 class="font-bold my-12">${title}</h1>
-                  ${content}
-              </div>
-          </div>
-  
-
-          ${contact()}
-  
-      </div>
-  
-      <!-- Pin to top right corner -->
-      <div class="absolute top-0 right-0 h-12 w-18 p-4">
-          <button class="js-change-theme focus:outline-none">🌙</button>
-      </div>
-  
-      <!-- jQuery if you need it
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    -->
-  
-      <script>
-          //Toggle mode
-          const toggle = document.querySelector('.js-change-theme');
-          const body = document.querySelector('body');
-          //const profile = document.getElementById('profile');
-  
-          toggle.addEventListener('click', () => {
-              if (body.classList.contains('text-gray-700')) {
-                  toggle.innerHTML = "☀️";
-                  body.classList.remove('text-gray-700');
-                  body.classList.add('text-gray-300');
-                  body.classList.remove('bg-gray-100');
-                  body.classList.add('bg-gray-900');
-              } else {
-                  toggle.innerHTML = "🌙";
-                  body.classList.remove('text-gray-300');
-                  body.classList.add('text-gray-700');
-                  body.classList.remove('bg-gray-900');
-                  body.classList.add('bg-gray-100');
-  
-              }
-          });
-      </script>
-  
-  </body>
-  
-  </html>`;
+exports.render = function ({ title, content }) {
+    return `
+        <div class="w-full md:w-1/2 order-1 md:order-2">
+            <div class="max-w-md leading-loose tracking-tight">
+                <h1 class="font-bold my-12">${title}</h1>
+                ${content}
+            </div>
+        </div>`;
 };
